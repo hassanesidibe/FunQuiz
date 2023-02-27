27a5fd75be0b862637e528e713fa2aa70450de0b
hassanesidibe <hassanesidibe5@gmail.com>
2023-02-27 11:14:35 -0500

    Initial Commit

---
+++ FunQuiz/Controller/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Hassane Sidibe on 2/25/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ FunQuiz/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FunQuiz
+//
+//  Created by Hassane Sidibe on 2/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //iPhone 14 Pro Max screen size:   430x932 pt (1290x2796 px @3x)
+
+
+}
+
